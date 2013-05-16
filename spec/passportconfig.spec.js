72cab63f04138646403d6f74e9aae8d16439ffa3
Ben McCormick <ben.mccormick@alumni.duke.edu>
2013-05-16 11:23:48 -0400

    I'm apparrently bad at git.  JS files deleted, should have been renamed to coffee and diffed in last commit :( I don't have the courage to try and figure out how to save this...

--- spec/passportconfig.spec.js
+++
@@ -1,8 +0,0 @@
-//For right now just check that it passes an object along acceptably
-var passportconfig = require("../src/passportconfig");
-
-describe("Passport Configurer", function() {
-  it("returns a passport instance", function() {
-    expect(passportconfig.configuredpassport).toBeDefined();
-  });
-});
